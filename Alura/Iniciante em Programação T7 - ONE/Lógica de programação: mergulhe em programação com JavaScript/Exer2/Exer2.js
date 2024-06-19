2095356f7a7fa577ed267508234f97dc689b200a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-19 18:27:54 -0300

    2

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer2/Exer2.js
@@ -0,0 +1,11 @@
+/*A variável que pode acontecer mudança do número secreto é let numeroSecreto = 4-*/
+
+
+alert('Boas-vindas ao jogo do número secreto')
+let chute = prompt('Escolha um número entre 1 e 10')
+
+let numeroSecreto = 4
+
+if (chute == numeroSecreto) {
+   alert('Acertou')
+}
