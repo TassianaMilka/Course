520834b13e4cdc8c764f26919b99d5c1a6c56a9f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-19 19:36:07 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer3/Exer3.js
@@ -0,0 +1,23 @@
+
+/*O bug está na condição do if (senha = senhaDoSistema) o comparador de igualdades são dois e não um.*/ 
+
+/*Erro*/ 
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha = senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}
+
+
+/*Certo*/ 
+let senhaDoSistema = "senhaTeste!";
+
+let senha = prompt("Digite a senha do sistema:");
+
+if (senha == senhaDoSistema){
+    alert("Acesso ao sistema garantido");
+}
+
+
