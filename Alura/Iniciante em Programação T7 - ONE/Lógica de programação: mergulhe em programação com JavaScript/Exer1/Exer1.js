4f0314c05d74b7fa4f521fa2ace26a3cde030783
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-19 17:47:42 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer1/Exer1.js
@@ -0,0 +1,5 @@
+ /*Vai exibir a mensagemDeBoasVindas na caixa e contém um erro pelo fato que não está
+/* exibindo da forma certa a variável na função alert.*/
+
+let mensagemDeBoasVindas = 'Boas vindas ao App';
+alert('mensagemDeBoasVindas');
