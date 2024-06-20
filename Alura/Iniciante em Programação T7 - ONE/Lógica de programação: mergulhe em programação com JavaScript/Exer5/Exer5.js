d1b5625c4b61f38ce4df07ba746d0ccb14a9d007
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 13:50:23 -0300

    Create Exer5.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer5/Exer5.js
@@ -0,0 +1,15 @@
+
+/* A concatenação +  encontra-se  na afirmativa dessa forma na alternativa diferente alert('O número secreto era ' + numeroSecreto + ', mas você escolheu ' + chute); */
+
+alert('Bem-vindo ao jogo do número secreto')
+let chute = prompt('Escolha um número entre 1 e 10')
+
+let numeroSecreto = 4
+
+console.log(chute == numeroSecreto)
+if (chute == numeroSecreto) {
+    alert('Acertou')
+} else {
+    alert('O número secreto era ' + numeroSecreto)
+}
+
