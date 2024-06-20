2d88dd85a2107dddaf652d0b78288eebcacb00ed
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 13:55:32 -0300

    1

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer6/Exer6.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer6/Exer6.js
@@ -1,6 +1,6 @@
 /* Se é igual a 18 pode começar dirigir ou é  menor que 18 não pode pelo fato ser de menor sua idade*/
 
-let idade = prompt("Digite sua idade:");
+let idade = prompt("Digite sua idade:"); 
 
 if (idade >= 18) {
   console.log("Você é maior de idade.");
