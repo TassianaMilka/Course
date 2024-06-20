3659ca276194083dbcc421ecef5f3c5ab897d77e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 13:58:51 -0300

    Create Exer7.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer7/Exer7.js
@@ -0,0 +1,25 @@
+/*Exemplo*/
+let porcentagemDesconto = 0;
+
+if(quantidadeMilhas > 5000){
+    porcentagemDesconto = porcentagemDesconto + 10;
+}
+
+if(quantidadeMilhas >= 30000){
+    porcentagemDesconto = porcentagemDesconto + 20;
+} else {
+    porcentagemDesconto = 0;
+}
+
+/* Mudança*/
+
+let porcentagemDesconto = 0;
+
+if(quantidadeMilhas >= 30000){
+    porcentagemDesconto = porcentagemDesconto + 20;
+} else {
+    if(quantidadeMilhas > 5000){
+    porcentagemDesconto = porcentagemDesconto + 10;
+}
+
+}
