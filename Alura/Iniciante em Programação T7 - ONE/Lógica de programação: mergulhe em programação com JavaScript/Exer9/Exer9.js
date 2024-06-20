c05cbc8944a894e975703cda99cc3097762181d9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 15:09:40 -0300

    Create Exer9.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer9/Exer9.js
@@ -0,0 +1,67 @@
+/* Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".*/
+
+
+let dia=1;
+
+let dia=promt console.log('Informe  se  é para  Sábado ou Domingo ou  número aleatório');
+
+  if(dia==1){
+  alert(' Sábado  e Domingo');
+}else{
+   alert(' Bom fim de semana!');
+
+}
+
+
+
+
+/*Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.*/
+
+
+let num=1;
+
+console.log('Resolução' );
+  if(num>=0){
+  alert('O número é maior que zero é  posiivo');
+}else if (num<=0) {
+   
+  alert('O número é menor que zero´é negativo');
+
+}
+
+
+
+/*Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".*/
+
+let num= 50;
+
+if(num>=100){
+    
+    console.log('Parabéns, você venceu');
+}else{
+    
+        console.log('Tente novamente para ganhar.');
+
+}*/
+
+/*Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.*/
+
+let saldo=400;
+
+alert('O seu valor de saldo ${saldo}');
+
+
+
+
+/*Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.*/
+
+
+let nome=" Tassiana";
+
+let nome=promt ('Informe  o seu nome');
+
+console.log(' O nome do usuário'+nome);
+
+
+
+
