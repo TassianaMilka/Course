711c422f5c9cae9ba02eb20ca5ff47e7041c474f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 14:13:11 -0300

    1

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer7/Exer7.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer7/Exer7.js
@@ -5,7 +5,7 @@ let porcentagemDesconto = 0;
 
 if(quantidadeMilhas > 5000){
     porcentagemDesconto = porcentagemDesconto + 10;
-}
+} 
 
 if(quantidadeMilhas >= 30000){
     porcentagemDesconto = porcentagemDesconto + 20;
