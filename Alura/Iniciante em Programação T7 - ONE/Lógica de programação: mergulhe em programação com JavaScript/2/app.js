b677123176d420db91dba3333a651621ad8ee9e1
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-20 13:46:31 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/2/app.js
@@ -0,0 +1,26 @@
+alert('Olá a todos esse é o primeiro exemplo de Javascript do jogo');
+
+/*Os nomes precisa ser claro para compreender*/ 
+/* Os número não é descrito com aspa pelo fato que é só utilizado para texto*/ 
+let  numeroSecreto =29;
+console.log(numeroSecreto)
+let chute=prompt('Escolha um número entre 1 e 31');
+
+/*Condição*/ 
+
+if(chute==numeroSecreto){
+
+alert('Você descobiru o número secreto ${numeroSecreto} ');
+
+}else{
+
+    alert('Você errou!')
+
+}
+
+/*Aclopamento*/ 
+
+/*Templete String*/ 
+
+/*Live Sever*/ 
+
