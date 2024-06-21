909209d7f6bc92206b506e2a1819ccc0630db6ce
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-21 15:11:45 -0300

    Create Exer12.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer12/Exer12.js
@@ -0,0 +1,27 @@
+/*Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.*/
+
+let num1=1;
+/*Loop*/
+while(num1<10){
+    
+    console.log('Número-'+num1);
+
+    num1++;
+    
+}
+
+/*Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.*/
+
+let num2=10;
+/*Loop*/
+while(num2>0){
+    
+    console.log('Número-'+num2);
+
+    num2--;
+     
+    
+}
+
+
+
