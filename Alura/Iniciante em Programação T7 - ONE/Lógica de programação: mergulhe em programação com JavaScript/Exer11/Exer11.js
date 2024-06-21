00f8a4fa072dfc4de8e4532cd0148a67bc8a578c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-21 12:13:53 -0300

    1

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer11/Exer11.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer11/Exer11.js
@@ -1,1 +1,32 @@
 
+/*Loop infinito*/
+
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
+
+/* Colocar na linha 11 o contatot -- pelo fato ele vai decrementar o contador a cada iteração,ou seja o zero(0) é positivo e precisa encontrar até o final. */
+
+let qtdNumeros = prompt('Digite a quantidade de números para o cálculo da média:');
+let soma = 0;
+let contador = qtdNumeros;
+
+while(contador > 0){
+    let numero = parseInt(prompt('Digite o numero:'));
+    soma += numero;
+
+   contator--;
+}
+
+let media = soma / qtdNumeros;
+
+console.log(media);
