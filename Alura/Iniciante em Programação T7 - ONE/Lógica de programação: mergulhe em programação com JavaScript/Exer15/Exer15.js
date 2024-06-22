2c731cc67fcfeb86f1d58c47be13d150ea34f669
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 18:07:48 -0300

    1

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
@@ -1,1 +1,16 @@
+/*Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.*/
+console.log("Boas-Vindas");
 
+
+/* Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.*/
+
+
+let nome="Tassiana";
+
+console.log("Olá,"+nome);
+
+/*Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .*/
+
+let nome1="Tassiana";
+
+alert("Olá,"+nome);
