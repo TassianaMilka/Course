017406a5ded557c2fdc76e378a31fb65559926bd
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 16:40:50 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer14/Exer14.js
@@ -0,0 +1,16 @@
+
+/*  Utilizando sem  o operador ternário*/
+
+
+let palavraPessoa = "";
+
+if(quantidadePessoas == 1){
+    palavraPessoa = "pessoa";
+}else{
+    palavraPessoa = "pessoas"
+} 
+
+
+/*Utilizando com operador ternário*/
+
+  let palavraPessoa=quantidadePessoas==1 ?'pessoa':'pessoas';
