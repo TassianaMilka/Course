c9fcd0961100f5908c34f6c00da77361f20f7654
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 18:13:34 -0300

    1

--- Alura/7Days of Code/Lógica com Javascript/2/Two.js
+++ Alura/7Days of Code/Lógica com Javascript/2/Two.js
@@ -45,3 +45,16 @@ if(programar==1){
 
         
     }
+/*Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.*/
+
+
+let linguagem="Javascript";
+
+
+let linguagem=prompt("Qual a linguagem de programação que você mais gosta?);
+
+console.log("Você gosta de estudar"+linguagem);
+
+
+
+
