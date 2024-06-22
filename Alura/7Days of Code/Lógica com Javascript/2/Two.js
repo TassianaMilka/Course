5f89fa881cdd94a73d245ce35802d5d15a403a48
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 14:12:37 -0300

    Update Two.js

--- Alura/7Days of Code/Lógica com Javascript/2/Two.js
+++ Alura/7Days of Code/Lógica com Javascript/2/Two.js
@@ -19,7 +19,7 @@ let linguagem;
 alert("Olá [nome], você tem [idade] anos e já está aprendendo [linguagem]!");
 
 
-/*Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.*/
+/*Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
 
 E aí, dependendo da resposta, ele deve mostrar uma das seguintes mensagens:
 
