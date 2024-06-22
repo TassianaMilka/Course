33ab9b3893b17bb6d83b02841ee91929b538ac11
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 19:32:04 -0300

    Update Exer15.js

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
@@ -91,6 +91,17 @@ if(numero>0){
 }
 
 
+/*Use um loop while para imprimir os números de 1 a 10 no console.*/
+
+
+let num=1;
+
+while(num<10){
+    
+    console.log("O número de"+num );
+    
+    num++;
+}
 
 
 
