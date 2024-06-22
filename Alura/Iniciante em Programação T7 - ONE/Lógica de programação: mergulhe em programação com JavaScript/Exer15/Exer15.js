f608dc205d0e2a2e9464c1ad4627d03b728dcedf
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 19:30:04 -0300

    Update Exer15.js

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
@@ -67,6 +67,30 @@ else if( idade<18){
 }
 
 
+/*Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.*/
+
+
+
+let numero;
+
+
+let numero = prompt("Digite o numero:"); 
+
+
+if(numero>0){
+    
+    console.log("O número é positivo");
+    
+}else if(numero<0){
+    
+    console.log("O número é negativo");
+}else{
+    
+        console.log("O número é zero");
+
+}
+
+
 
 
 
