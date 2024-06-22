d02ff17e31670b748d23ad3f5bae72f56f16e766
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-22 18:30:19 -0300

    Update Exer15.js

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
@@ -33,4 +33,18 @@ console.log("O resultado de resultado="+resultado);
 
 
 
+/*Crie uma variável chamada "valor1" e outra chamada "valor2", atribuindo a elas valores numéricos de sua escolha. Em seguida, realize a subtração desses dois valores e armazene o resultado em uma terceira variável chamada "resultado". Utilize o console.log para mostrar a mensagem "A diferença entre [valor1] e [valor2] é igual a [resultado]." no console.*/
+
+
+
+let valor1=2;
+let valor2=3;
+let resultado;
+
+resultado=valor1-valor2;
+
+console.log("O valor 1="+valor1);
+console.log("O valor 2="+valor2);
+console.log("O resultado de resultado="+resultado);
+
 
