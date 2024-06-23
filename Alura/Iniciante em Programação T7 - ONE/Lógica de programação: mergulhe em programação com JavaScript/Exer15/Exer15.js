387fc2ca46c1e2be6ddf0a00b29f7fb51b5566ad
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-23 06:46:57 -0300

    Update Exer15.js

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: mergulhe em programação com JavaScript/Exer15/Exer15.js
@@ -144,5 +144,14 @@ console.log(numero);
 
 
 
+/*Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.*/
 
+let numero2= parseInt(Math.random() * 10) + 1;
+console.log(numero2);
+
+/*Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.*/
+
+
+let numero3= parseInt(Math.random() * 1000) + 1;
+console.log(numero3);
 
