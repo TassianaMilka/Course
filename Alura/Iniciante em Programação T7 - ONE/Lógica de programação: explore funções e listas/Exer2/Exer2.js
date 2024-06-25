b1dfff7f3ab6550ab9c50420aced4a5c585347ce
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-25 15:06:58 -0300

    Create Exer2.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer2/Exer2.js
@@ -0,0 +1,9 @@
+
+As funções em JavaScript podem ter um nome;
+Palavra-chave function: É usada para declarar uma função em JavaScript;
+Parênteses (): Após o nome da função (se houver), os parênteses são usados para definir os parâmetros da função.
+
+function (nome){
+
+  
+}
