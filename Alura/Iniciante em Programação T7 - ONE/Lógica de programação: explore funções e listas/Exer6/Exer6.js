b1bce40f05ff73bed9fa7faab2d85b2c0c5bad2e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-25 18:29:27 -0300

    1

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer6/Exer6.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer6/Exer6.js
@@ -6,11 +6,13 @@ let nota2 = 6;
 let nota3 = 3;
 let nota4 = 5;*/
 
+/*1*/
 
 function verificarAprovacao(media) {
     return media >= 5 ? "Aprovado" : "Reprovado";
 }
 
+/*2*/
 
 function calcularMedia(nota1, nota2, nota3, nota4) {
   let media = (nota1 + nota2 + nota3 + nota4) / 4;
