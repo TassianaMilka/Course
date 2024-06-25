58ddcbb1b554457034a4e4fd6a6a90d7dacf8b6c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-25 16:21:51 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer5/Exer5.js
@@ -0,0 +1,9 @@
+
+/*O que aprendeu ?*/
+
+/*Aprendi  pelo JavaScript  para gerar número aleatório dentro da função dessa maneira :*/
+function  gerarumNumeroAleatorio( ){
+
+ return parseInt(Math.random()*10+1);
+  
+ /*A diferença das funções dentro do parênteses com nome  e sem nome.*/
