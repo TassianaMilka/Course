cf55fbf79578ca0511af06bb0edb976dd05e9086
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-25 15:39:30 -0300

    Update Exer4.js

--- Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer4/Exer4.js
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer4/Exer4.js
@@ -10,3 +10,18 @@ function imprimirmensagem(){
 imprimirmensagem();
 
 
+/*Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.*/
+
+function imprimirmensagem(nome){
+
+nome="Tassiana";
+
+    console.log("Olá,"+nome);
+    
+  
+}
+
+imprimirmensagem();
+
+
+
