28ed23c3d2c25a93afdd04a48b75283c17051757
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-26 16:25:50 -0300

    Create Exer7.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer7/Exer7.js
@@ -0,0 +1,12 @@
+/*Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.*/
+
+
+function calculaIMC(peso,altura){
+
+imc = 56 / (1.90* 1.90);
+
+}
+calculaIMC();
+console.log("\n" +imc);
+
+
