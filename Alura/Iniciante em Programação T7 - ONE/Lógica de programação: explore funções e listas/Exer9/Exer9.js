01a585863f3d7a60ff642cd07c22d0596610cd21
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-27 16:52:11 -0300

    Create Exer9.js

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer9/Exer9.js
@@ -0,0 +1,31 @@
+/*Crie uma lista vazia, com o nome listaGenerica.*/
+
+let lista=[];
+
+console.log(lista);
+
+/*Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.*/
+let linguagensDeProgramacao=['JavaScript','C', 'C++','kotlin','Python'];
+
+//console.log( linguagensDeProgramacao);*/
+/*Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.*/
+let linguagensDeProgramacao=['JavaScript','C', 'C++','kotlin','Python'];
+linguagensDeProgramacao.push('Java, Ruby, GoLang');
+console.log(linguagensDeProgramacao);
+/*Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.*/
+let nome=["Tassiana","Milka","Tassi"];
+
+
+console.log(nome[0]);
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.*/
+let nome1=["Tassiana","Milka" ,"Tassi"];
+
+
+console.log(nome1[1]); 
+
+/*Crie uma lista com 3 nomes e exiba no console apenas o último elemento.*/
+
+let nome2=["Tassiana","Milka" ,"Tassi"];
+
+console.log(nome2[2]); 
