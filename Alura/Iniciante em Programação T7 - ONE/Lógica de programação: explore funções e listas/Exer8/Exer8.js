43d0790044efd4dea36dc17d6f7b48139edf15d5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-06-27 16:22:55 -0300

    1

---
+++ Alura/Iniciante em Programação T7 - ONE/Lógica de programação: explore funções e listas/Exer8/Exer8.js
@@ -0,0 +1,10 @@
+/* O listaCompras.length exibe a quantidade de elementos de uma lista e o pão vai esta classificado em terceiro na lista. */
+
+
+let listaCompras = [];
+
+listaCompras.push("Maçã");
+listaCompras.push("Arroz");
+listaCompras.push("Leite");
+listaCompras.push("Pão");
+listaCompras.push("Carne");
