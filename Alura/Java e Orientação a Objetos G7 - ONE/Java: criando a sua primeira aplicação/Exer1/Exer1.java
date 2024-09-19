a4f940baceba25fdb4b9ddb207ff9cace3936c2b
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-19 18:07:11 -0300

    Create Exer1.java

---
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer1/Exer1.java
@@ -0,0 +1,10 @@
+//O método main foi declarado de maneira incorreta  e o  programa apresentará um erro de compilação.
+
+
+public class Exemplo {
+
+  public void main(String[] args) {
+    System.println("Oi, meu nome é Alura!");
+  }
+
+}
