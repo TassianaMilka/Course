eff8cb2ed73cd07cdf05c9ea7abdd97b369e8ef1
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-19 18:19:33 -0300

    1

--- Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer3/Perfil/src/Perfil.java
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer3/Perfil/src/Perfil.java
@@ -1,4 +1,3 @@
-
 //Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil, com o método main, que ao executar
 // deverá imprimir um cumprimento personalizado no console. Por exemplo, "Olá, [Seu Nome]!".
 //Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".
