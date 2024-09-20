0fcca6cd4e55b4774bc023bb32d19305f9944d1e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-20 14:39:14 -0300

    1

---
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer6/Notas/src/Main.java
@@ -0,0 +1,16 @@
+//Crie um programa que realize a média de duas notas decimais e exiba o resultado.
+public class Main {
+    public static void main(String[] args) {
+
+        double nota1 = 4.5;
+        double nota2 = 8.5;
+        double  media = (nota1 + nota2) / 2;
+
+
+
+        System.out.println("Média das notas\n"+media);
+
+
+
+    }
+}
