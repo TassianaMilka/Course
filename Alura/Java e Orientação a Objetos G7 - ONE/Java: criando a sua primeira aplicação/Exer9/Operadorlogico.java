8c00f9b18562ceb86bd37ee6e202fe3e7db706a0
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-22 00:25:38 -0300

    Create Operadorlogico.java

---
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer9/Operadorlogico.java
@@ -0,0 +1,8 @@
+//O operador lógico AND (que corresponde ao E) é representado pelos caracteres && e o OR (que corresponde ao OU) pelo ||.
+
+
+//Os operadores lógicos devem ter no lado esquerdo e direito uma expressão booleana.
+
+
+System.out.println("And  ou");
+
