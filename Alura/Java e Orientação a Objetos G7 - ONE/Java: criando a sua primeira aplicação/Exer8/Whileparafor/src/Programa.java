4a932e0b868c8900594757bee284362b6a11b53c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-09-22 00:23:42 -0300

    1

---
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: criando a sua primeira aplicação/Exer8/Whileparafor/src/Programa.java
@@ -0,0 +1,29 @@
+
+//While
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
+
+//to
+
+
+//For
+
+
+public class Programa {
+    public static void main(String[] args) {
+        for(int contador = 1; contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
