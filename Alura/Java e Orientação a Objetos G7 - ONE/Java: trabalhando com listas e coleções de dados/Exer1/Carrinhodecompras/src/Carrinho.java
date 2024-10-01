311f03ae679817b8e7717522700a846654b35132
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-10-01 15:18:41 -0300

    1

---
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: trabalhando com listas e coleções de dados/Exer1/Carrinhodecompras/src/Carrinho.java
@@ -0,0 +1,21 @@
+//O método size, da classe ArrayList, serve justamente para indicar a
+// quantidade atual de elementos da lista.O método size, da classe ArrayList,
+// serve justamente para indicar a quantidade atual de elementos da lista.
+
+//public class Carrinho {
+//
+//  ArrayList<Produto> produtos = new ArrayList<>();
+//
+//  public void adicionaProduto(Produto produto) {
+//    produtos.add(produto);
+//  }
+
+public class Carrinho {
+
+    ArrayList<Produto> produtos = new ArrayList<>();
+
+    public void adicionaProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+}
