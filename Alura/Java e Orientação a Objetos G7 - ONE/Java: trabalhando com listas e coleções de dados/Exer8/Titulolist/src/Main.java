df71b077ddb8c5adffae1059ca6fd58da70e4956
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-10-10 20:51:41 -0300

    1

--- Alura/Java e Orientação a Objetos G7 - ONE/Java: trabalhando com listas e coleções de dados/Exer8/Titulolist/src/Main.java
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: trabalhando com listas e coleções de dados/Exer8/Titulolist/src/Main.java
@@ -3,7 +3,7 @@
 //Crie uma classe Titulo com um atributo nome do tipo String.
 // Implemente a interface Comparable na classe para que seja
 // possível ordenar uma lista de objetos Titulo.
-
+ 
 import java.util.ArrayList;
 import java.util.Collections;
 
