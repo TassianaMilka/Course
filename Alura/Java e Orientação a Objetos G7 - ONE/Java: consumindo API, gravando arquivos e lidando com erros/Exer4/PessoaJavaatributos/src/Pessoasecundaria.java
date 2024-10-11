3035364dcfd39613d523f1a0260f1a976fa3f7d9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-10-11 18:16:18 -0300

    1

--- Alura/Java e Orientação a Objetos G7 - ONE/Java: consumindo API, gravando arquivos e lidando com erros/Exer4/PessoaJavaatributos/src/Pessoasecundaria.java
+++ Alura/Java e Orientação a Objetos G7 - ONE/Java: consumindo API, gravando arquivos e lidando com erros/Exer4/PessoaJavaatributos/src/Pessoasecundaria.java
@@ -1,7 +1,7 @@
 //Modifique o programa do Exercício anterior para permitir a conversão de
 //um JSON mesmo se alguns campos estiverem ausentes ou se houver campos
 //adicionais não representados no objeto Pessoa. Consulte a documentação
-//da biblioteca Gson para flexibilizar a conversão.
+//da biblioteca Gson para flexibilizar a conversão. 
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
