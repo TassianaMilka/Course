735e72076df5e6581b129ebb78faec3d8239d70c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-11-06 19:52:42 -0300

    1

--- Alura/Java e Spring Framework G7 - ONE/Exer2/GenericRepositorio.java
+++ Alura/Java e Spring Framework G7 - ONE/Exer2/GenericRepositorio.java
@@ -1,7 +1,7 @@
 /* utiliza de forma correta o conceito de tipos genéricos, possibilitando que o repository salve diferentes tipos de dados, 
 diminuindo a repetição de código.*/
 
-public class GenericRepository<T> {
+public class GenericRepository<T> { 
     public T save(T t) {
         // Código para salvar 't' no banco de dados
     }
