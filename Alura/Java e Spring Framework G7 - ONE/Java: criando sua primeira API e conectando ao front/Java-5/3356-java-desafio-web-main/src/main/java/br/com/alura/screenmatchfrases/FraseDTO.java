8d2c0b9a5d1e26e3ced29736c2e068ead028c52a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-12-04 21:09:24 -0300

    Delete Alura/Java e Spring Framework G7 - ONE/Java: criando sua primeira API e conectando ao front/Java-5 directory

--- Alura/Java e Spring Framework G7 - ONE/Java: criando sua primeira API e conectando ao front/Java-5/3356-java-desafio-web-main/src/main/java/br/com/alura/screenmatchfrases/FraseDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.screenmatchfrases;
-
-public record FraseDTO( String titulo,
-                        String frase,
-                        String personagem,
-                        String poster) {
-}
