9b326b1ca36e04e074de08a0e7b59d61e7d98af4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-12-24 14:14:35 -0300

    1

---
+++ Alura/Java e Spring Framework G7 - ONE/Spring Boot 3: documente, teste e prepare uma API para o deploy/Exer1/Query.java
@@ -0,0 +1,11 @@
+
+/*Faltou adicionar o caractere dois-pontos (:) antes do parâmetro data.*/
+
+@Query(“””
+    select p from Produto p
+    where
+    p.preco >= :preco
+    and
+    p.dataCadastro >= data
+“””)
+List<Produto> buscarPorPrecoEData(BigDecimal preco, LocalDate data);
