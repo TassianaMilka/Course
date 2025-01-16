3855480389c58b34778e12a3d9cc9c0eb9df09b4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-16 14:33:06 -0300

    1

--- loiane.training/Java Basic/4/leituraDadosTeclado.java
+++ loiane.training/Java Basic/4/leituraDadosTeclado.java
@@ -3,7 +3,7 @@
 
 public class leituraDadosTeclado{//Nome da classe
 
-   public static void main(String[] args){
+   public static void main(String[] args){ 
 
     Scanner scan=new Scanner(System.in);
 
