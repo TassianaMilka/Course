c3ab9b6509e7a27599faf4ce6d0b7b7d99f3f775
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-16 14:32:08 -0300

    Update Operadores.java

--- loiane.training/Java Basic/5/Operadores.java
+++ loiane.training/Java Basic/5/Operadores.java
@@ -1,1 +1,49 @@
+public class OperadoresAritmeticos{
 
+
+  public static void main(String[] args){
+
+    int resultado=1+2;
+
+    System.out.println(resultado);
+
+
+    resultado = resultado-1;   
+    System.out.println(resultado);
+
+    resultado=resultado*2;
+    System.out.println(resultado);
+
+
+    resultado=resultado/2;
+    System.out.println(resultado);
+    
+    resultado=resultado+8;
+    System.out.println(resultado);
+
+    resultado=resultado%7;
+    System.out.println(resultado);
+
+
+    String primeiroNome="Está é";
+    String segundoNome="Uma String concatenada.";
+    String terceiroNome=primeiroNomee+segundoNome;
+    System.out.println(terceiroNome);
+
+
+    resultado=resultado+1;
+    System.out.println(resultado+);
+    resultado++;
+    System.out.println(resultado);
+    System.out.println(resultado ++);
+    System.out.println(resultado);
+    resultado=resultado+1;
+    resultado+=1;
+
+    //Outras partes de explicação foi sobre:
+
+    //Operadores de ==,<> ou !.
+    
+  }
+
+}
