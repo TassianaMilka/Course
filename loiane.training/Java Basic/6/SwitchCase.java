5e661f3b2b3b971c9510015aaf046b085f8184e7
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-17 22:25:28 -0300

    Update SwitchCase.java

--- loiane.training/Java Basic/6/SwitchCase.java
+++ loiane.training/Java Basic/6/SwitchCase.java
@@ -1,1 +1,60 @@
 
+public class SwitchCase{
+
+public static void main(String[] args){
+
+
+   Scanner scan = New Scanner(System.in);
+
+   System.out.println("Informe um dia da semana de [1 até 7]");
+   int diaSemana= scan.nextInt();
+
+
+  if(diaSemana == 1){
+
+    System.out.println("Domingo");
+    
+
+  } else if(diaSemana == 2){
+
+
+     System.out.println("Segunda");
+
+   
+} else if(diaSemana == 3){
+
+
+     System.out.println("terça");
+
+   
+} else if(diaSemana == 4){
+
+
+     System.out.println("Quarta");
+
+   
+} else if(diaSemana == 5){
+
+
+     System.out.println("Quinta");
+
+   
+} else if(diaSemana == 6){
+
+
+     System.out.println("Sexta");
+
+   
+} else if(diaSemana == 7){
+
+
+     System.out.println("Sábado");
+
+   }else{
+
+     System.out.println("Não é um dia válido);
+
+  }
+
+
+}
