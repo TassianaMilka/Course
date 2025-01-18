2d561904f5ee96a6b3e5285a2b9960a46ea42411
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-17 22:31:55 -0300

    Update SwitchCase.java

--- loiane.training/Java Basic/6/SwitchCase.java
+++ loiane.training/Java Basic/6/SwitchCase.java
@@ -56,5 +56,43 @@ public static void main(String[] args){
 
   }
 
+  switch(diaSemana){
+
+   case 1:
+          System.out.println("Domingo");
+   break;
+        
+   case 2:
+          System.out.println("Segunda");
+   break;  
+        
+   case 3:
+          System.out.println("Terça");
+   break;
+        
+   case 4:
+          System.out.println("Quarta");
+   break;
+        
+   case 5:
+          System.out.println("Quinta");
+   break;
+        
+   case 6:
+          System.out.println("Sexta");
+   break;
+        
+   case 7:
+          System.out.println("Sábado");
+
+   break;
+        
+   default:
+        
+         System.out.println("Não é um dia da semana válida");
+        
+        
+}
+
 
 }
