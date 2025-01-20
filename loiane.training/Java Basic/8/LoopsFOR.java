b005f556d65dd3cbce3be7cf3559df0b470b9b68
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-19 21:16:29 -0300

    1

--- loiane.training/Java Basic/8/LoopsFOR.java
+++ loiane.training/Java Basic/8/LoopsFOR.java
@@ -1,1 +1,24 @@
 
+public  class LoopsFOR{
+
+  public static void main(String[] args){
+
+  
+     for(int i=0;i <5; i++){
+
+
+       System.out.println("i tem valor"+i);
+
+      }
+
+      for(int i=0;i >=0; i++){
+
+
+       System.out.println("i tem valor"+i);
+
+        }
+
+
+     }
+
+}
