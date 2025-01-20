96bf85f57bb56a0615c7c6a3f9b0442eedfa3e5f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-01-20 18:38:40 -0300

    1

---
+++ loiane.training/Java Basic/9/Break.java
@@ -0,0 +1,34 @@
+
+
+ public  class Break{
+
+  public static void main(String[] args){
+
+  
+    Scanner scan= new Scanner(System.in);
+
+
+    System.out.println("Entre com um número");
+    int num = scan.nextInt();
+
+    
+  
+    System.out.println("Entre com um número");
+    in tmax = scan.nextInt();
+
+      
+     for(int i=num;i >=max; i++){
+
+       if(i%7==0){
+
+       System.out.println("i tem valor"+i);
+
+        break;
+         
+
+             }
+          }
+   
+     }
+
+}
