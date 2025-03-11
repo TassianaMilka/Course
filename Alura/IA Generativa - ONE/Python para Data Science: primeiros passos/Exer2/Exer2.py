dd348ec3f890732e8f70e961c536acc036a43f66
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 15:52:39 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer2.py
@@ -9,7 +9,6 @@ print('A soma!')
 
 print(nump+nums)
 
-
 ######
 
 
@@ -27,6 +26,24 @@ numt = int (input('Informe o terceiro número:'))
 print('A soma!')
 
 print(nump+nums+numt)
+
+######
+
+
+
+
+#3-Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a subtração do primeiro pelo o segundo valor.
+
+
+nump = int (input('Informe o primeiro número:'))
+
+nums = int (input('Informe o segundo número:'))
+
+
+print('A subtração!')
+
+print(nump-nums)
+
 
 ######
 
