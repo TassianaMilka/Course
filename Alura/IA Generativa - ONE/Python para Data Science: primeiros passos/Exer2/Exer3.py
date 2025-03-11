12924a65eae87954feb65e1186d2b9e2e2c02848
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 17:18:37 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer3.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer3.py
@@ -35,3 +35,30 @@ print(frase_minuscula)
 ######
 
 
+#5-Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “e” trocadas pela letra “f”.
+
+frase = input('Informe uma frase com vogais que contém a letra "e":')
+novo_texto = frase.replace("e", "f")
+print(novo_texto)
+
+######
+
+
+#6-Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “a” trocadas pela caractere “@”.
+
+frase = input('Informe uma frase com vogais que contém a letra "e":')
+novo_texto = frase.replace("a", "@")
+print(novo_texto)
+
+######
+
+
+#7-Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as consoantes “s” trocadas pelo caractere “$”.
+
+
+frase = input('Informe uma frase com vogais que contém a letra "e":')
+novo_texto = frase.replace("s", "$")
+print(novo_texto)
+
+
+
