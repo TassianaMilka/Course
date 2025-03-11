9345831228a22d0d8e1fbf2f7311ca34f4bf2085
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 16:55:14 -0300

    Update Exer3.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer3.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer3.py
@@ -1,1 +1,8 @@
 
+
+#1-Crie uma variável chamada “frase” e atribua a ela uma string de sua escolha. Em seguida, imprima a frase na tela.
+
+frase = input('Informe uma frase:')
+
+print(f'{frase}')
+
