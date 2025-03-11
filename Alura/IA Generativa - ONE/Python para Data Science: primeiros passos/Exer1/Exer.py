f83f84f7c93b01b1217aeaeecc0f19cc5ead5883
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 15:22:02 -0300

    1

---
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer1/Exer.py
@@ -0,0 +1,36 @@
+#1-Imprima a frase Escola de Dados da Alura!.
+
+#2-Imprima seu nome e seu sobrenome seguindo a estrutura abaixo:
+
+
+print('Escola de Dados da Alura!')
+print('Tassiana Milka!')
+
+######
+
+
+#3-Imprima o seu primeiro nome letra a letra. Por exemplo, meu nome #é Mirla, então eu obtenho a seguinte saída:
+
+print('T')
+print('A')
+print('S')
+print('S')
+print('I')
+print('A')
+print('N')
+print('A')
+
+######
+
+
+#4-Imprima o dia do seu nascimento em formato dia mês ano. #Lembrando que os valores de dia e ano não podem estar entre #aspas. Supondo uma data de aniversário dia 28 de fevereiro de
+ #2003, o formato deve estar como no exemplo abaixo:
+
+print('21 de Outubro')
+
+
+######
+
+#5-Imprima, em um único print, o atual ano que você está fazendo #esse curso. O valor do ano deve ser um dado numérico e a saída #do print deve ser a seguinte:
+
+print('Ano atual:2025')
