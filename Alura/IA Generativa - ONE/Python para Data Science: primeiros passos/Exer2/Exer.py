3f93e8324cd09612bb16f508d24a0c685c2957c5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 15:40:09 -0300

    Create Exer.py

---
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer.py
@@ -0,0 +1,31 @@
+
+#Crie um programa que solicite à pessoa usuária digitar seu nome, e imprima “Olá, [nome]!”.
+
+nome = input('Informe seu nome:')
+print(f'Olá,{nome}')
+
+######
+
+#Crie um programa que solicite à pessoa usuária digitar seu nome e idade, e imprima “Olá, [nome], você tem [idade] anos.”.
+
+nome = input('Informe seu nome:')
+idade = int (input('Informe sua idade:'))
+print(f'Olá,{nome}')
+print(f'Sua idade:{idade}')
+
+######
+
+
+#Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.
+
+
+
+nome = input('Informe seu nome:')
+idade = int (input('Informe sua idade:'))
+altura = float (input('Informe sua altura:'))
+print(f'Olá,{nome} você tem {idade} anos e mede {altura} metros! ')
+
+
+######
+
+
