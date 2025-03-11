f28bb2bb263d2e52a8eb0a295b00090a120c7600
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-11 15:51:06 -0300

    Create Exer2.py

---
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer2/Exer2.py
@@ -0,0 +1,32 @@
+
+#1-Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a soma dos dois valores.
+
+nump = int (input('Informe o primeiro número:'))
+
+nums = int (input('Informe o segundo número:'))
+
+print('A soma!')
+
+print(nump+nums)
+
+
+######
+
+
+
+#2-Crie um programa que solicite três valores numéricos à pessoa usuária e imprima a soma dos três valores.
+
+
+nump = int (input('Informe o primeiro número:'))
+
+nums = int (input('Informe o segundo número:'))
+
+
+numt = int (input('Informe o terceiro número:'))
+
+print('A soma!')
+
+print(nump+nums+numt)
+
+######
+
