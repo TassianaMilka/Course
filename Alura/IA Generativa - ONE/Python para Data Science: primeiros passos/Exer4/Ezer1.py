5676201b432181c4d3ea4318de4acf896fa56b91
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-12 18:03:28 -0300

    Create Ezer1.py

---
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Ezer1.py
@@ -0,0 +1,11 @@
+
+#1-Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
+
+
+for contador in range(0,2):
+
+  num=int(input('Informe o número'))
+
+  print(num)
+
+
