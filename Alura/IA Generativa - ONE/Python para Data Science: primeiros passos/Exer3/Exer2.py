0bee826e609de55badd7a1ba023290464a17e0b2
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-12 17:54:35 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer3/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer3/Exer2.py
@@ -152,4 +152,24 @@ print(f'Valor a ser pago pelo cliente: R$ {valor_pago}')
 ######
 
 
+#13-Em uma empresa de venda de imóveis você precisa criar um código que analise os dados de vendas anuais para ajudar a diretoria na tomada de decisão. O código precisa coletar os dados de quantidade de venda durante os anos de 2022 e 2023 e fazer um cálculo de variação percentual. A partir do valor da variação, deve ser enviada às seguintes sugestões:
 
+#Para variação acima de 20%: bonificação para o time de vendas.
+#Para variação entre 2% e 20%: pequena bonificação para time de vendas.
+#Para variação entre 2% e -10%: planejamento de políticas de incentivo às vendas.
+#Para bonificações abaixo de -10%: corte de gastos.
+
+
+venda_p = float(input('Informe a primeira quantidade de vendas: '))
+venda_s = float(input('Informe a segunda quantidade de vendas: '))
+
+var_percentual = 100 * (venda_s - venda_p) / (venda_p)
+
+if var_percentual > 20:
+    print('Bonificação para o time de vendas.')
+elif 2 <= var_percentual <= 20:
+    print('Pequena bonificação para time de vendas.')
+elif -10 <= var_percentual < 2:
+    print('Planejamento de políticas de incentivo às vendas.')
+else:
+    print('Corte de gastos.')
