7d1a109624faeea2bb7bcea09a3190bd461ad7d4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-12 13:50:30 -0300

    Update Exer.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer3/Exer.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer3/Exer.py
@@ -1,1 +1,22 @@
 
+
+#1-Escreva um programa que peça à pessoa usuária para fornecer dois números e exibir o número maior.
+
+nump=int(input('Informe o primeiro número'))
+nums=int(input('Informe o segundo número'))
+
+
+if nump>nums:
+
+  print(f'O número maior é={nump}')
+
+elif nums>nump:
+
+  print(f'O número maior é={nums}')
+  
+
+######
+
+
+
+
