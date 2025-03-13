b154d3eef5fc0aa0438de416cfe47efe4c655251
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-13 15:29:41 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer2.py/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer2.py/Exer2.py
@@ -9,10 +9,34 @@ for i in range(1, 11):
     print(f'{num} x {i} = {resultado}')
 
 
+######
 
 
+#7-Os números primos possuem várias aplicações dentro da Ciência de Dados em criptografia e segurança, por exemplo. Um número primo é aquele que é divisível apenas por um e por ele mesmo. 
+#Assim, faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
+
+
+num = int(input('Informe um número: '))
+
+if num <= 1:
+    print('\n')
+else:
+    primo = True 
+    for i in range(2, num):
+        if num % i == 0:
+            primo = False  
+            break
+
+    if primo:
+        print('O número é primo!')
+    else:
+        print('O número não é primo
+
+
+              
 
 ######
 
 
 
+              
