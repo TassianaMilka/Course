1f4093ddedf6ea2118219d4f12081195a97b773e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-13 14:22:27 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer2.py/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer2.py/Exer2.py
@@ -1,1 +1,18 @@
 
+
+#6-Escreva um programa que gere a tabuada de um número inteiro de 1 a 10, de acordo com a escolha da pessoa usuária. Como exemplo, para o número 2, a tabuada.
+
+num = int(input('Informe um número inteiro de 1 a 10: '))
+
+for i in range(1, 11):
+    resultado = num * i
+    print(f'{num} x {i} = {resultado}')
+
+
+
+
+
+######
+
+
+
