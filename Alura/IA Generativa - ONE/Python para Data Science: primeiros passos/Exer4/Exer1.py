0913df1da02cb304aeb79504b86a1cea21b138de
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-13 14:18:15 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Exer1.py
@@ -2,7 +2,7 @@
 #1-Escreva um programa que peça dois números inteiros e imprima todos os números inteiros entre eles.
 
 
-for contador in range(0,2):
+for contador in range(0,2): 
 
   num=int(input('Informe o número'))
 
