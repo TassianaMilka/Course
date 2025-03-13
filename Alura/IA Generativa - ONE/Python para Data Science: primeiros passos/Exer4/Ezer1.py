92009ab6fd297c9e425e3c5baf43fca5b9bf4a13
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-13 13:59:30 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Ezer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exer4/Ezer1.py
@@ -9,3 +9,31 @@ for contador in range(0,2):
   print(num)
 
 
+
+#2-Escreva um programa para calcular quantos dias levará para a colônia de uma bactéria A ultrapassar ou igualar
+#a colônia de uma bactéria B, com base nas taxas de crescimento de 3% e 1,5% respectivamente. Considere que a colônia 
+#A inicia com 4 elementos e a B com 10.
+
+colonia_a = 4
+colonia_b = 10
+
+
+dias = 0
+
+
+while colonia_a <= colonia_b:
+
+  colonia_a=colonia_a * 1 + 0.03
+  colonia_b=colonia_b* 1 + 0.015
+
+  dias += 1
+
+
+print(f'Irá levar {dias} dias para a colônia A ultrapassar a colônia B.')
+
+
+
+######
+
+
+
