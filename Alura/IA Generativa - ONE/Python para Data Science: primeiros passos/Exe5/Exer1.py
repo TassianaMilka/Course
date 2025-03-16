b6f8b62b12ed717fc6b7dbae2fe9a4d22bbce29f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 00:46:57 -0300

    Update Exer1.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer1.py
@@ -1,1 +1,40 @@
 
+#1-Faça um programa que tenha a seguinte lista contendo os valores de gastos de uma empresa de papel [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]. Com esses valores, 
+#faça um programa que calcule a média de gastos. Dica: use as funções built-in sum() e len().
+
+
+valor=[2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08];
+
+
+len(valor)
+
+
+
+######
+
+
+valor=[2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08];
+
+
+sum(valor)
+
+
+
+######
+
+
+#2-
+
+
+
+
+
+
+
+######
+
+#3-Faça um código que colete em uma lista 5 números inteiros quaisquer e imprima a lista. Exemplo: [1,4,7,2,4].
+
+numeros=[1,4,7,2,4];
+
+print(numeros)
