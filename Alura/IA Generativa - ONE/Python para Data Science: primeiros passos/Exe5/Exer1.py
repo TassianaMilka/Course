1f3046888c10bc1a40ae5deb2dc4478ee58a71cf
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 11:43:40 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer1.py
@@ -22,9 +22,24 @@ sum(valor)
 
 ######
 
-#2-
 
- 
+#2-Com os mesmos dados da questão anterior, defina quantas compras foram realizadas acima de 3000 reais e calcule a porcentagem quanto ao total de compras.
+
+
+ valor = [2172.54, 3701.35, 3518.09, 3456.61, 3249.38, 2840.82, 3891.45, 3075.26, 2317.64, 3219.08]
+
+
+compras_acima_3000 = [v for v in valor if v > 3000]
+quantidade_acima_3000 = len(compras_acima_3000)
+
+
+porcentagem = (quantidade_acima_3000 / len(valor)) * 100
+
+
+print(f"A quantidade de compras acima de 3000 reais é: {quantidade_acima_3000}")
+print(f"A porcentagem de compras acima de 3000 reais é: {porcentagem:.2f}%")
+
+
 
 ######
 
