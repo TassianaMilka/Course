aeffedcc003117e4b796de0b63f345035b6d6969
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 14:20:58 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
@@ -21,7 +21,7 @@ print(f'Porcentagens de crescimento:\n{porcentagem_crescimento}')
 ######
 
 
-8-
+#8-
 
 
 
@@ -53,3 +53,21 @@ print(f'Nota final: {soma}')
 
 
 ######
+
+
+#10-Um instituto de meteorologia deseja fazer um estudo de temperatura média de cada mês do ano. Para isso, você precisa fazer um código que colete e armazene essas temperaturas médias em uma lista. Depois, calcule a média anual das 
+#temperaturas e mostre todas as temperaturas acima da média anual e em que mês elas ocorreram, mostrando os meses por extenso (Janeiro, Fevereiro, etc.).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
