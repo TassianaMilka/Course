c61a17d14f4f6cf1765fac20cb4eafdcd5618166
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 15:37:54 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
@@ -98,9 +98,27 @@ for i in range(0,12):
 
 ######
 
+#11-Uma empresa de e-commerce está interessada em analisar as vendas dos seus produtos. Os dados das vendas foram armazenados em um dicionário:
+
+dados_vendas = {'Produto A': 300, 'Produto B': 80, 'Produto C': 60, 'Produto D': 200, 'Produto E': 250, 'Produto F': 30}
+
+total_vendas = 0;
+produto_mais_vendido = '';
+unidades_produto_mais_vendido = 0;
+
+#keys permite a utilização para trabalhar os dados dentro do dicionário
+
+for produto in dados_vendas.keys():
+
+  total_vendas += dados_vendas[produto]
+ 
+  if dados_vendas[produto] > unidades_produto_mais_vendido:
+    unidades_produto_mais_vendido = dados_vendas[produto]
+    produto_mais_vendido = produto
+
+print(f'Total de vendas é {total_vendas}')
+print(f'{produto_mais_vendido} é o mais vendido')
 
 
 
-
-
-
+#12- 
