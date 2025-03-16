fd7f516e3a13dfee55ec2b152e7c01df651419a7
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 15:31:38 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
@@ -79,10 +79,24 @@ print(f'Nota final: {soma}')
 
 
 
+temperaturas_mensais = []
+for i in range(0,12):
+  temperaturas_mensais.append(float(input(f'Informe a média de temperatura do mês: ')))
+
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+media_anual = sum(temperaturas_mensais) / len(temperaturas_mensais)
 
 
+print('Temperaturas acima da média em: ')
+for i in range(0,12):
+ 
+  if temperaturas_mensais[i] > media_anual:
+
+    print(meses[i])
 
 
+######
 
 
 
