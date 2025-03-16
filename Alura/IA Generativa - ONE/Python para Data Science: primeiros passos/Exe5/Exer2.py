eb8cd7e80f6b78b771c2310fb9d94d5094d67449
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-03-16 15:28:17 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: primeiros passos/Exe5/Exer2.py
@@ -21,10 +21,29 @@ print(f'Porcentagens de crescimento:\n{porcentagem_crescimento}')
 ######
 
 
-#8-
+#8-Para uma seleção de produtos alimentícios, precisamos separar o conjunto de IDs dados por números inteiros sabendo que os produtos com ID par são doces e os com ID ímpar são amargos. Monte um código que colete 10 IDs. 
+#Depois, calcule e mostre a quantidade de produtos doces e amargos.
 
 
+ids = []
+doce = 0
+amargo = 0
 
+for i in range(0,5):
+
+  ids.append(int(input(f'Informe o ID: ')))
+
+for id in ids:
+
+  if id % 2 == 0:
+
+    doce += 1
+  else:
+    amargo += 1
+
+
+print(f'Quantidade de produtos doces: {doce}')
+print(f'Quantidade de produtos amargos: {amargo}')
 
 
 ######
