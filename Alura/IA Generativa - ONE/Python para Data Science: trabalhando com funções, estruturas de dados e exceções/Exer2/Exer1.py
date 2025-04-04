b745044cc8a8ece3b91096651f9886af0ef25cc1
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-04 17:07:28 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer2/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer2/Exer1.py
@@ -62,7 +62,15 @@ tabuada(num)
 #4-Crie uma lista dos quadrados dos números da seguinte lista [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. Lembre-se de utilizar as funções lambda e map() para calcular o quadrado de cada elemento da lista.
 
 
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+quadrado = lambda x: x ** 2
+
+resultado = list(map(quadrado, numeros))
+
+#Imprimir o resultado da lista de multiplicação
+
+list(resultado)
 
 
 ######
