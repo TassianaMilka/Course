e6313ce1bfafefaed142abf3e11e5f811ce7c2c1
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-04 20:34:50 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer2/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer2/Exer2.py
@@ -1,1 +1,32 @@
+#5-Você foi contratado(a) como cientista de dados de uma associação de skate.
+#Para analisar as notas recebidas de skatistas em algumas competições ao longo do ano,
+#você precisa criar um código que calcula a pontuação dos(as) atletas. 
+#Para isso, o seu código deve receber 5 notas digitadas pelas pessoas juradas.
+
+
+notas = []
+
+for i in range(1,6):
+  nota = float(input(f"Informe a {i}ª nota: "))
+  notas.append(nota)
+
+def media(lista):
+  lista.remove(max(lista))
+  lista.remove(min(lista))
+  return sum(lista) / len(lista)
+
+media = media(notas)
+print(f"Nota da manobra: {round(media, 1)}") 
+
+
+######
+
+
+#6-Para atender a uma demanda de uma instituição de ensino para a análise do desempenho de seus(suas) estudantes, você precisa criar uma função que receba uma lista de 4 notas e retorne:
+
+#maior nota
+#menor nota
+#média
+#situação (Aprovado(a) ou Reprovado(a))
+#Para testar o comportamento da função, os dados podem ser exibidos em um texto:
 
