777383cfc026d78d0228f92a4ed2f1193c987702
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-05 15:02:07 -0300

    Update Exer1.py

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
@@ -13,3 +13,26 @@ for lista in lista_de_listas:
 
 #2-Crie um código para gerar uma lista que armazena o terceiro elemento de cada tupla contida na seguinte lista de tuplas:
 
+lista_de_tuplas = [('Pedro', 1.74, 81), ('Júlia', 1.65, 67), ('Otávio', 1.81, 83)];
+lista=[];
+
+for tupla in lista_de_tuplas:
+       lista.append(tupla[2])
+print(lista)
+
+
+######
+
+
+#3-A partir da lista: lista = ['Pedro', 'Júlia', 'Otávio', 'Eduardo'], crie um código para gerar uma lista de tuplas em que cada tupla tenha o primeiro 
+#elemento como a posição do nome na lista original e o segundo elemento sendo o próprio nome.
+
+
+
+
+
+######
+
+
+
+
