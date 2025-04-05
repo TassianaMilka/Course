4357ff40f6ca7551651217589e4ced23189fb26f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-05 15:24:02 -0300

    1

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
@@ -54,7 +54,16 @@ print(valores_apartamentos)
 ######
 
 
+#5-Crie um dicionário usando o dict comprehension em que as chaves estão na lista meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'] 
+#e os valores estão em despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360].
 
+
+meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+despesa = [860, 490, 1010, 780, 900, 630, 590, 770, 620, 560, 840, 360];
+
+despesas_mensais = {mes: valor for mes, valor in zip(meses, despesa)}
+
+print(despesas_mensais)
 
 
 ######
