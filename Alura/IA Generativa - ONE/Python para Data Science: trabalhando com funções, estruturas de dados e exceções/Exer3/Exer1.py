fd0b9c5ec2975c27de0c1f8e2039ac69edf4eea1
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-05 15:21:37 -0300

    Update Exer1.py

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer1.py
@@ -38,6 +38,22 @@ print(lista_de_tuplas)
 
 
 
+#4-Crie uma lista usando o list comprehension que armazena somente o valor numérico de cada tupla caso o primeiro elemento seja 'Apartamento', a partir da seguinte 
+#lista de tuplas:
+
+
+ aluguel = [('Apartamento', 1700), ('Apartamento', 1400), ('Casa', 2150), ('Apartamento', 1900), ('Casa', 1100)];
+
+
+
+valores_apartamentos = [valor for tipo, valor in aluguel if tipo == "Apartamento"]
+
+print(valores_apartamentos)
+
+
+######
+
+
 
 
 
