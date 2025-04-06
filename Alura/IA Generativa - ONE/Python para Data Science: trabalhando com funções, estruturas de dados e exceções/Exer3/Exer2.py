911bf6721ebc4b8af55361e3225818cc5c3f032e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-05 23:29:46 -0300

    Update Exer2.py

--- Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer2.py
+++ Alura/IA Generativa - ONE/Python para Data Science: trabalhando com funções, estruturas de dados e exceções/Exer3/Exer2.py
@@ -1,1 +1,60 @@
+#6-Uma loja possui um banco de dados com a informação de venda de cada representante e de cada ano e precisa filtrar somente os dados do ano 2022 com venda maior do
+#que 6000. A loja forneceu uma amostra contendo apenas as colunas com os anos e os valores de venda para que você ajude a realizar a filtragem dos dados a partir de
+#um código:
 
+#Crie uma lista usando list comprehension para filtrar os valores de 2022 e que sejam maiores que 6000.
+
+
+vendas = [('2023', 4093), ('2021', 4320), ('2021', 5959), ('2022', 8883), ('2023', 9859), ('2022', 5141), ('2022', 7688), ('2022', 9544), ('2023', 4794), ('2021', 7178), ('2022', 3030), ('2021', 7471), ('2022', 4226), ('2022', 8190), ('2021', 9680), ('2022', 5616)]
+
+
+filtro = [valor for ano, valor in vendas if ano == '2022' and valor > 6000]
+
+print(filtro)
+
+
+######
+
+
+
+#7-Uma clínica analisa dados de pacientes e armazena o valor numérico da glicose em um banco de dados e gostaria de rotular os dados da seguinte maneira:
+
+#Glicose igual ou inferior a 70: 'Hipoglicemia'
+#Glicose entre 70 a 99: 'Normal'
+#Glicose entre 100 e 125: 'Alterada'
+#Glicose superior a 125: 'Diabetes'
+#A clínica disponibilizou parte dos valores e sua tarefa é criar uma lista de tuplas usando list comprehension contendo o rótulo e o valor da glicemia em cada tupla.
+
+
+
+glicemia = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73]
+
+
+rotulos = [('Hipoglicemia', glicose) if glicose <= 70 else ('Normal', glicose) if glicose < 100 else ('Alterada', glicose) if glicose < 125 else ('Diabetes', glicose) for glicose in glicemia]
+
+print(rotulos)
+
+
+######
+
+#8-O e-commerce precisa estruturar esses dados em uma tabela contendo o valor total da venda, que é obtida multiplicando a quantidade pelo preço unitário. Além disso, a tabela precisa conter um cabeçalho indicando as colunas: 'id', 'quantidade', 'preco' e 'total'.
+
+#Crie uma lista de tuplas em que cada tupla tenha id, quantidade, preço e valor total, na qual a primeira tupla é o cabeçalho da tabela.
+
+
+id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+quantidade = [15, 12, 1, 15, 2, 11, 2, 12, 2, 4]
+preco = [93.0, 102.0, 18.0, 41.0, 122.0, 14.0, 71.0, 48.0, 14.0, 144.0]
+
+tabela = [('id', 'quantidade', 'preco', 'total')]
+tabela += [(id[i], quantidade[i], preco[i], quantidade[i]*preco[i]) for i in range(len(id))]
+
+print(tabela)
+
+
+######
+
+#9-
+
+
+######
