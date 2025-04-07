fb4865fb374e0bf358336f36e1b16b408ea3f380
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-07 14:57:19 -0300

    1

--- Alura/IA Generativa - ONE/Análise de dados com Python: utilizando o ChatGPT como assistente/Exer1/Exer1.py
+++ Alura/IA Generativa - ONE/Análise de dados com Python: utilizando o ChatGPT como assistente/Exer1/Exer1.py
@@ -1,1 +1,20 @@
+#Um colega da equipe de dados da Zoop solicitou a análise de outro conjunto de dados além do enviado pela liderança. 
+#Esses dados se referem às vendas totais obtidas por 896 franquias no primeiro mês do trimestre de análise da Zoop.
+
+
+import pandas as pd
+import numpy as np
+import requests  
+
+url_franquias = 'https://raw.githubusercontent.com/alura-cursos/python-analise-chatgpt-assistente/main/Dados/vendas_mensal_franquias.json'
+
+response = requests.get(url_franquias)
+data = response.json()
+
+df = pd.DataFrame(data)
+
+
+df
+
+
 
