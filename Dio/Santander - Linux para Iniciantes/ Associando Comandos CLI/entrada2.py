c70d06d0be4662505550b51ca9e9ae012add53dd
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-11 16:19:43 -0300

    1

---
+++ Dio/Santander - Linux para Iniciantes/ Associando Comandos CLI/entrada2.py
@@ -0,0 +1,20 @@
+# Recebe a entrada do usuário e armazena na variável "entrada"
+entrada = input()
+
+# Função responsável por receber um conceito e retornar sua respectiva descrição.
+def descrever_conceito(conceito):
+  if conceito == "chgrp":
+    return "Altera o grupo associado a arquivos ou diretórios"
+
+# COMPLETE AQUI: Preencha corretamente cada conceito, considerando as descrições abaixo:
+  elif conceito == "chmod":
+    return "Altera permissões de um arquivo ou diretório"
+
+  elif conceito == "chown":
+    return "Altera o proprietário de um arquivo"
+
+  elif conceito == "umask":
+    return "Ajusta as permissões padrão para novos arquivos e diretórios"
+
+# Imprime a descrição do conceito recebido na "entrada" através da função "descrever_conceito". 
+print(descrever_conceito(entrada))
