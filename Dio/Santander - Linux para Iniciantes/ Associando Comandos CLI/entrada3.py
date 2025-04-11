744a9ab4ea3fd8de56b6fba0623747b4d8d0407a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-11 16:20:16 -0300

    1

---
+++ Dio/Santander - Linux para Iniciantes/ Associando Comandos CLI/entrada3.py
@@ -0,0 +1,21 @@
+
+# Recebe a entrada do usuário e armazena na variável "entrada"
+entrada = input()
+
+# Função responsável por receber um conceito e retornar sua respectiva descrição.
+def descrever_conceito(conceito):
+  if conceito == "alias":
+    return "Cria um atalho ou um nome alternativo para um comando"
+
+# COMPLETE AQUI: Preencha corretamente cada conceito, considerando as descrições abaixo:
+  elif conceito == "echo":
+    return "Usado para imprimir saída"
+
+  elif conceito == "exit":
+    return "Encerrar a sessão atual do shell ou o script"
+
+  elif conceito == "history":
+    return "Fornece uma lista de comandos executados anteriormente"
+
+# Imprime a descrição do conceito recebido na "entrada" através da função "descrever_conceito". 
+print(descrever_conceito(entrada))
