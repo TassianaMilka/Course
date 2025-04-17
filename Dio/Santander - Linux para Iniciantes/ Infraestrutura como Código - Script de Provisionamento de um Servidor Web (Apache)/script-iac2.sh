744aafce1162611dd37689bfba016367255a7465
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-04-16 22:54:15 -0300

    Update script-iac2.sh

--- Dio/Santander - Linux para Iniciantes/ Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)/script-iac2.sh
+++ Dio/Santander - Linux para Iniciantes/ Infraestrutura como Código - Script de Provisionamento de um Servidor Web (Apache)/script-iac2.sh
@@ -1,1 +1,17 @@
 
+#1/bin/bash
+
+echo "Atualização o servidor ..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo"Baixando e copiando os arquivos da aplicação"
+
+cd/ temp
+wget link
+wzip main.zip
+cd linux-site-dio-main
+cp -R* var/www/html/
