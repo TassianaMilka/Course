5a769d0417dbee15dced72bb6fb6946bc0dcdafa
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-05-11 18:59:35 -0300

    Create Exer1.py

---
+++ Dio/Microsoft 50 Anos - Computação em Nuvem com Azure/ Primeiros Passos na Nuvem/Exer1/Exer1.py
@@ -0,0 +1,18 @@
+
+# Recebe a entrada do usuário e armazena na variável "entrada":
+entrada = input()
+
+# Função responsável por receber um conceito e retornar sua respectiva descrição:
+def descrever_servico(servico):
+  if servico == "IaaS":
+    return "Infraestrutura como serviço"
+
+# COMPLETE AQUI: Preencha corretamente cada conceito, considerando as descrições abaixo: 
+  elif servico == "PaaS":
+    return "Plataforma como serviço"
+  elif servico == "SaaS":
+    return "Software como serviço"
+  elif servico == "FaaS":
+    return "Função como serviço, baseada em eventos"
+
+print(descrever_servico(entrada))
