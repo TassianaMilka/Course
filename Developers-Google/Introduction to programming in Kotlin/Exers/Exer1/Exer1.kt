0deddd73e46bf99807d7dca15e547e73a93aa36f
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 11:45:06 -0300

    Update Exer1.kt

--- Developers-Google/Introduction to programming in Kotlin/Exers/Exer1/Exer1.kt
+++ Developers-Google/Introduction to programming in Kotlin/Exers/Exer1/Exer1.kt
@@ -56,3 +56,23 @@ fun main() {
 
 
 
+/*4-Para praticar a solução de problemas, corrija os erros nos exercícios a seguir. Para cada exercício, copie o código no Playground Kotlin no navegador. Ao tentar executar o programa, você vai ver uma mensagem de erro.
+
+Corrija o erro do programa para que ele gere a saída esperada.
+
+fun main() {
+    println("Tomorrow is rainy")
+Saída esperada:
+
+
+Tomorrow is rainy*/
+
+
+
+fun main() {
+    println("Tomorrow is rainy")
+
+}
+
+
+
