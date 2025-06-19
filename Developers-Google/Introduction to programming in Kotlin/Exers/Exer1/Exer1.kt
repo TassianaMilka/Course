3e0e25f59a30b3ae25a7b86a2523093a1c33040c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 11:37:59 -0300

    Create Exer1.kt

---
+++ Developers-Google/Introduction to programming in Kotlin/Exers/Exer1/Exer1.kt
@@ -0,0 +1,9 @@
+#1-Você consegue ler o código deste programa e adivinhar qual vai ser a saída (sem precisar executar no Playground Kotlin)?
+
+
+
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
