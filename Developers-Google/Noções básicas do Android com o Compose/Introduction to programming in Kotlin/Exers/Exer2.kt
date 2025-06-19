9d7c4b6c31733c45c115592b370fbe7520d04f82
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 14:01:46 -0300

    Update Exer2.kt

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer2.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer2.kt
@@ -1,4 +1,16 @@
+/*1-Print number*/
+
+
 fun main() {
     val count: Int = 3
     println(count)
+}
+
+
+
+/*2-Print message about number one*/
+
+fun main() {
+    val count: Int = 1
+    println("You have $count unread messages.")
 }
