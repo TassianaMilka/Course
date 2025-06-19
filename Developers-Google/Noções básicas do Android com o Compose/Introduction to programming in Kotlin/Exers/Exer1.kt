f86c373f3273264d4c6b56319f19c26c90133b09
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 13:55:01 -0300

    1

---
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer1.kt
@@ -0,0 +1,131 @@
+/*1-Você consegue ler o código deste programa e adivinhar qual vai ser a saída (sem precisar executar no Playground Kotlin)?*/
+
+
+
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
+
+
+
+/*2-Use o Playground Kotlin para criar um programa que gere as mensagens abaixo:
+I'm
+learning
+Kotlin!*/
+
+
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+
+
+/*3-Copie e cole esse programa no Playground Kotlin.
+
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
+Corrija o programa para que ele mostre esta saída:
+
+
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday*/
+
+
+
+fun main() {
+    
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+  
+}
+
+
+
+/*4-Para praticar a solução de problemas, corrija os erros nos exercícios a seguir. Para cada exercício, copie o código no Playground Kotlin no navegador. Ao tentar executar o programa, você vai ver uma mensagem de erro.
+
+Corrija o erro do programa para que ele gere a saída esperada.
+
+fun main() {
+    println("Tomorrow is rainy")
+Saída esperada:
+
+
+Tomorrow is rainy*/
+
+
+
+fun main() {
+    println("Tomorrow is rainy")
+
+}
+
+
+/*5-Corrija o erro do programa para que ele gere a saída esperada.
+
+fun main() {
+    printLine("There is a chance of snow")
+}
+Saída esperada:
+
+
+There is a chance of snow*/
+
+
+
+fun main() {
+    println("There is a chance of snow")
+
+}
+
+
+
+/*6-Corrija o erro do programa para que ele gere a saída esperada.
+
+fun main() {
+    println("Cloudy") println("Partly Cloudy") println("Windy")
+}
+Saída esperada:
+
+
+Cloudy
+Partly Cloudy
+Windy*/
+
+
+fun main() {
+    println("Cloudy") 
+    println("Partly Cloudy") 
+    println("Windy")
+}
+
+
+/*7-Corrija o erro do programa para que ele gere a saída esperada.
+
+fun main() (
+    println("How's the weather today?")
+)
+Saída esperada:
+
+
+How's the weather today?*/
+
+
+fun main() {
+    println("How's the weather today?") 
+  
+}
