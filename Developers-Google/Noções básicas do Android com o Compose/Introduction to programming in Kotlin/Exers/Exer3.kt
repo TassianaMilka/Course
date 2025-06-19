91c14a7d0517dc89ac3062311c18350789e7fd86
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 14:33:36 -0300

    1

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer3.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer3.kt
@@ -1,1 +1,42 @@
 
+
+/*1-Function defined to print menssages*/
+fun main() {
+    birthdayGreeting()
+}
+
+fun birthdayGreeting() {
+    println("Happy Birthday, Rover!")
+    println("You are now 4 years old!")
+}
+
+
+
+/*2-Example code about app*/
+
+fun main() {
+    birthdayGreeting()
+}
+
+fun birthdayGreeting(): Unit {
+    println("Happy Birthday, Rover!")
+    println("You are now 5 years old!")
+}
+
+
+
+/*3-Print very string with menssages*/
+
+fun birthdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
+
+fun main() {
+    println(birthdayGreeting("Rover"))
+}
+
+
+
