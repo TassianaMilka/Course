d7627288132bafad9ea1a566f46ff32dca33e34c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 14:46:08 -0300

    Update Exer4.kt

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
@@ -68,6 +68,42 @@ fun main() {
 
 
 
+/*4-Este programa mostra o número total de pessoas em uma festa. Ele inclui adultos e crianças. A variável numberOfAdults contém o número de adultos na festa, e a variável numberOfKids contém o número de crianças.
+
+
+fun main() {
+    val numberOfAdults = "20"
+    val numberOfKids = "30"
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
+Etapa 1
+Você consegue determinar a saída desse programa antes de executar o código no Playground Kotlin?
+Depois de determinar a saída, execute o código no Playground Kotlin e verifique se ela corresponde à que foi mostrada.
+
+Dica: o que acontece quando você usa o operador + em duas strings?
+
+Etapa 2
+O código funciona e mostra alguns resultados, mas não o número total de pessoas participando.
+
+Você consegue encontrar e corrigir o problema no código para que ele gere este resultado?
+
+The total party size is: 50*/
+
+
+
+fun main() {
+    val numberOfAdults = 20;
+    val numberOfKids = 30;
+    val total = numberOfAdults + numberOfKids;
+    
+    println("The total party size is: $total");
+}
+
+
+
+
+
 
 
 
