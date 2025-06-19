49dca04d7ca667bbb5790bf7bb8ff75204dc6830
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 14:50:01 -0300

    Update Exer4.kt

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
@@ -103,7 +103,31 @@ fun main() {
 
 
 
+/*5-Este programa mostra o salário total que um funcionário recebe no mês. O salário total é dividido em duas partes: a variável baseSalary, que o funcionário recebe todos os meses, e a variável bonusAmount, que é um bônus adicional concedido ao funcionário.
 
+
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = "$baseSalary + $bonusAmount"
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+Você consegue descobrir a saída desse código antes de o executar no Playground Kotlin?
+Quando você executa o código no Playground Kotlin, ele gera a saída esperada?*/
+
+
+
+
+fun main() {
+    
+    val baseSalary = 5000;
+    val bonusAmount = 1000;
+    val totalSalary = baseSalary + bonusAmount;
+
+    
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).");
+    
+}
 
 
 
