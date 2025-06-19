1ddf460cebd819fe1552f47702b3dbe3db7e3707
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-19 11:41:59 -0300

    Update Exer1.kt

--- Developers-Google/Introduction to programming in Kotlin/Exers/Exer1/Exer1.kt
+++ Developers-Google/Introduction to programming in Kotlin/Exers/Exer1/Exer1.kt
@@ -24,4 +24,35 @@ fun main() {
 
 
 
-/*3-  */ 
+/*3-Copie e cole esse programa no Playground Kotlin.
+
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
+Corrija o programa para que ele mostre esta saída:
+
+
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday*/
+
+
+
+fun main() {
+    
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+  
+}
+
+
+
