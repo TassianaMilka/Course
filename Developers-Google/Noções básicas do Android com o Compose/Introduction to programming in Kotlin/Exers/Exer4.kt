835cccdc37489ff9ed609e0c050d75d817a773d9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-20 13:24:22 -0300

    Update Exer4.kt

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
@@ -270,4 +270,53 @@ fun displayAlertMessage(operatingSystem : String, emailId: String): String {
 
 }
 
+/*Etapa 2
+Muito bem! Você mostrou a mensagem. No entanto, em alguns casos, você vai perceber que não é possível determinar o sistema operacional do usuário. Nessas situações, é preciso especificar o nome do sistema operacional como Unknown OS. É possível otimizar ainda mais o código para que não seja necessário transmitir o argumento Unknown OS sempre que a função for chamada.
+
+Você consegue encontrar uma maneira de otimizar o código com essas informações para que ele gere a saída abaixo?
+
+There's a new sign-in request on Unknown OS for your Google Account user_one@gmail.com.
+
+There's a new sign-in request on Windows for your Google Account user_two@gmail.com. 
+
+There's a new sign-in request on Mac OS for your Google Account user_three@gmail.com. 
+Para mostrar a mensagem acima, substitua a implementação da função main() por esta:
+
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId. 
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(emailId = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}*/
+
+
+fun main() {
+    val operatingSystem = "Chrome OS"
+    val emailId = "sample@gmail.com"
+
+    println(displayAlertMessage(operatingSystem, emailId))
+}
+
+
+fun displayAlertMessage(operatingSystem : String, emailId: String): String {
+    
+    return "There's a new sign-in request on $operatingSystem for your Google Account $emailId."
+
+}
+
+
 
