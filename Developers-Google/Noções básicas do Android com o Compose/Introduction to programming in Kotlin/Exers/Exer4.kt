3cc7ae46c43ee019e32053ded144a95ef75113c9
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-20 19:19:59 -0300

    Update Exer4.kt

--- Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Introduction to programming in Kotlin/Exers/Exer4.kt
@@ -359,3 +359,28 @@ String {
     return "$firstUserEmailId,$secondUserOperatingSystem,$secondUserEmailId ,$thirdUserOperatingSystem and $thirdUserEmailId."
 
 }
+
+
+/*O pedômetro é um dispositivo eletrônico que conta o número de passos. Atualmente, quase todos os smartphones, smartwatches e equipamentos de ginástica vêm com pedômetros integrados. O app de saúde e fitness usa pedômetros
+integrados para calcular esse número. Essa função calcula o número de calorias que o usuário queima com base nos passos dados.*/
+
+
+
+fun PEDOMETERstepsTOcalories(NumberOFStepS: Int): Double {
+    
+    val CaloriesBURNEDforEachStep = 0.04
+    val TotalCALORIESburned = NumberOFStepS * CaloriesBURNEDforEachStep
+    return TotalCALORIESburned
+    
+}
+
+fun main() {
+    
+    val Steps = 4000
+    val caloriesBurned = PEDOMETERstepsTOcalories(Steps);
+    println("Walking $Steps steps burns $caloriesBurned calories") 
+    
+}
+
+
+
