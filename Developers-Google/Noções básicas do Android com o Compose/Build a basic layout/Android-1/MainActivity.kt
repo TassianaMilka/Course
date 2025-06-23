9c50cc1608b1d8eea6668de2e0faae552b8325df
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-06-22 21:54:53 -0300

    1

--- Developers-Google/Noções básicas do Android com o Compose/Build a basic layout/Android-1/MainActivity.kt
+++ Developers-Google/Noções básicas do Android com o Compose/Build a basic layout/Android-1/MainActivity.kt
@@ -1,1 +1,61 @@
+package com.example.myapplication_1
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication_1.ui.theme.MyApplication1Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplication1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    //Color and example
+    Surface(color=Color.LightGray){
+
+        Text(
+            text = "Hi, my name is  $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Composable
+fun Surface(content: () -> Unit) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplication1Theme {
+        Greeting("Android")
+    }
+}
