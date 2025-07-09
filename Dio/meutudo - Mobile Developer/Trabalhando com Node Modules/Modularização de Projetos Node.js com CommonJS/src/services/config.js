7c09250dec3ba7c7bbd44d459a2ed995426179a4
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-07-09 14:16:19 -0300

    1

---
+++ Dio/meutudo - Mobile Developer/Trabalhando com Node Modules/Modularização de Projetos Node.js com CommonJS/src/services/config.js
@@ -0,0 +1,18 @@
+
+//Configurar sobre o ambiente de desenvolvimento do cliente
+const devArea={
+
+  version:"01ab",
+  production:false,
+};
+
+const client = {
+  device: "webDeveloper",
+
+};
+
+//Modulo do dados
+module.exports = {
+  devArea,
+  client,
+};
