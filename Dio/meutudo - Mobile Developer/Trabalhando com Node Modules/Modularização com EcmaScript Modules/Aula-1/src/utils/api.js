4c123206126da4930d06932791a0edfe39af0d0a
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-07-10 17:15:16 -0300

    1

---
+++ Dio/meutudo - Mobile Developer/Trabalhando com Node Modules/Modularização com EcmaScript Modules/Aula-1/src/utils/api.js
@@ -0,0 +1,14 @@
+//Exportar a value da chave de uma api para seu funcionamento pela permissão do
+//administrador
+
+export const key={
+  value:"#",
+  permission:"admin",
+
+};
+
+
+export const getDataFromApi =() =>{
+
+  console.log("dados da api sendo buscado");
+}
