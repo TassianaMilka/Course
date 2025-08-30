6ecbf36fd0d52961226b73cca826a1c1a59bc13c
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-08-30 16:05:28 -0300

    Add files via upload

---
+++ Dio/meutudo - Mobile Developer/Introdução a React Native/Primeiros Passos com React Native & Expo/Aula/App.tsx
@@ -0,0 +1,49 @@
+
+//Importação dos arquivos
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
+
+//Exportação da função do App
+export default function App() {
+  const [isActive, setisActive] = useState(false)
+
+  function handleSymbol(){
+    
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+
+    console.log(isActive)
+  }
+
+    //Visualização dos  símbolos na tela
+  return (
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol} >
+          <Image 
+            source={isActive ? symbolOn : symbolOff}
+          />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+//Etilo de  cor de fundo,centralizar a letra e imagem
+
+const styles = StyleSheet.create({
+  containerOn: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerOff:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
