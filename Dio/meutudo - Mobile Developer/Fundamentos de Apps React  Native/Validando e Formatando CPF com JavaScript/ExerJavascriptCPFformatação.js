1747499f8cea5d8008f01ae5fbca3045c2d2a7a5
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-09-12 22:40:41 -0300

    Update ExerJavascriptCPFformatação.js

--- Dio/meutudo - Mobile Developer/Fundamentos de Apps React  Native/Validando e Formatando CPF com JavaScript/ExerJavascriptCPFformatação.js
+++ Dio/meutudo - Mobile Developer/Fundamentos de Apps React  Native/Validando e Formatando CPF com JavaScript/ExerJavascriptCPFformatação.js
@@ -1,1 +1,16 @@
 
+const valor = gets();
+
+function formatarCPF(cpf) {
+  const apenasNumeros = cpf.replace(/\D/g, '');
+
+  if (apenasNumeros.length !== 11) {
+    return "CPF invalido";
+  }
+
+  const cpfFormatado = apenasNumeros.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+
+  return cpfFormatado;
+}
+
+print(formatarCPF(valor));
