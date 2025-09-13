eae1054c610c49a8027ae8a3002da721a64abe4e
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2025-09-12 22:22:26 -0300

    Update ExerJavascriptCPF.js

--- Dio/meutudo - Mobile Developer/Fundamentos de Apps React  Native/Validando e Formatando CPF com JavaScript/ExerJavascriptCPF.js
+++ Dio/meutudo - Mobile Developer/Fundamentos de Apps React  Native/Validando e Formatando CPF com JavaScript/ExerJavascriptCPF.js
@@ -1,1 +1,35 @@
 
+
+
+const valor = gets()
+
+function validarCPF(cpf) {
+
+  cpf = cpf.replace(/\D/g, '');
+  
+  if(cpf.length !== 11){
+    
+   return false;
+   
+  }
+  
+  if((/^(/d)\1{10}$/.test(cpf))){
+    
+    
+   return false;
+   
+  }
+  
+  if(cpf.endsWith("00")){
+    
+    
+  return false;
+  
+  }
+  
+  return true;
+  
+}
+
+
+print(validarCPF(valor));
